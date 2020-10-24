7a5bb83ca3d99d928e72fb8433777a0fd2e2f9bf
Mateusz Kiełbasa <sobriodev@gmail.com>
2020-10-24 10:00:20 +0200

    Add unity test framework library
    Add file ut.h which contains declarations of all tests across the library
    Add file ut_runner.c which run unit tests

---
+++ unit_test/ut.h
@@ -0,0 +1,19 @@
+#ifndef UT_H
+#define UT_H
+
+#include "unity.h"
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+/* -------------------------------------------------------------------------- */
+/* ---------------------------- Test declarations --------------------------- */
+/* ------------ Naming convention: UT_{MODULE}_{FUNCTION}_{CONDITION} ------- */
+/* -------------------------------------------------------------------------- */
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif // UT_H
---
+++ unit_test/ut_runner.c
@@ -0,0 +1,19 @@
+#include "ut.h"
+#include "unity.h"
+
+#include <stdio.h>
+
+int main(void)
+{
+    UNITY_BEGIN();
+    return UNITY_END();
+}
+
+void setUp()
+{
+    /* There must be setUp definition */
+}
+void tearDown()
+{
+    /* There must be tearDown definition */
+}
